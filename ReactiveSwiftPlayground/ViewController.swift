d654aadfa2b55b991d25f2c1307c387fa6d65ff4
Julian Caicedo <julian@7mind.de>
2020-03-20 11:42:31 +0100

    Empty project

---
+++ ReactiveSwiftPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReactiveSwiftPlayground
+//
+//  Created by Julian Caicedo on 20.03.20.
+//  Copyright © 2020 juliancadi.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
